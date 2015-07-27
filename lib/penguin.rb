a3fdbce7f88c3b48fa4152db0c50869d49463029
zkusznir <zkusznir@gmail.com>
2015-07-27 11:35:51 +0200

    Define middleware and 'X-RateLimit-Limit' header

--- lib/penguin.rb
+++ lib/penguin.rb
@@ -1,5 +1,18 @@
 require "penguin/version"
 
 module Penguin
-  # Your code goes here...
+  class Middleware
+    attr_accessor :app
+    
+    def initialize(app, options = {})
+      @app = app
+      @limit = options[:limit]
+    end
+
+    def call(env)
+      status, headers, body = @app.call(env)
+      headers['X-RateLimit-Limit'] = @limit
+      [status, headers, body]
+    end
+  end
 end
--- spec/penguin_spec.rb
+++ spec/penguin_spec.rb
@@ -1,16 +1,24 @@
 require 'spec_helper'
 require 'rack/test'
-require 'pry'
+require 'penguin'
 
 describe Penguin do
   include Rack::Test::Methods
 
-  def app
-    lambda { |env| ['200', {'Content-Type' => 'text/html'}, ["Hey there!\n"]] }
-  end
+  let(:app) {
+    Penguin::Middleware.new(
+      lambda { |env| ['200', {'Content-Type' => 'text/html'}, ["Hey!\n"]] },
+      {limit: 50})
+  }
+
+  before(:each) { get '/' }
 
   it 'gets successful response' do
-    get '/'
-    expect last_response.ok?
+    expect(last_response.ok?).to be true
+    expect(last_response).to include('X-RateLimit-Limit')
+  end
+
+  it 'sets limit rate from parameters' do
+    expect(last_response.header['X-RateLimit-Limit']).to eq(50)
   end
 end

1474b7eb3a88ac55e0d2b6b8be3a98c5659d5ae2
zkusznir <zkusznir@gmail.com>
2015-07-28 13:33:44 +0200

    Decrease remaining limit

--- lib/penguin.rb
+++ lib/penguin.rb
@@ -5,12 +5,15 @@ module Penguin
     
     def initialize(app, options = {})
       @app = app
-      @limit = options[:limit].to_s
+      @limit = options[:limit]
     end
 
     def call(env)
       status, headers, body = @app.call(env)
-      headers['X-RateLimit-Limit'] = @limit
+      @limit_remaining ||= @limit
+      @limit_remaining -= 1
+      headers['X-RateLimit-Limit'] = @limit.to_s
+      headers['X-RateLimit-Remaining'] = @limit_remaining.to_s
       [status, headers, body]
     end
   end
--- spec/penguin_spec.rb
+++ spec/penguin_spec.rb
@@ -5,22 +5,29 @@ require 'penguin'
 describe Penguin do
   include Rack::Test::Methods
 
+  let(:limit) { 50 }
+
   let(:app) {
     Rack::Lint.new(
       Penguin::Middleware.new(
         lambda { |env| ['200', {'Content-Type' => 'text/html'}, ["Hey!\n"]] },
-        {limit: 50})
+        {limit: limit})
     )
   }
 
   before(:each) { get '/' }
 
   it 'gets successful response' do
-    expect(last_response.ok?).to be true
-    expect(last_response).to include('X-RateLimit-Limit')
+    expect(last_response).to be_ok
   end
 
   it 'sets limit rate from parameters' do
-    expect(last_response.header['X-RateLimit-Limit']).to eq('50')
+    expect(last_response.header['X-RateLimit-Limit']).to eq(limit.to_s)
+  end
+
+  it 'decreases limit rate' do
+    expect(last_response.header['X-RateLimit-Remaining'].to_i).to eq(limit-1)
+    get '/'
+    expect(last_response.header['X-RateLimit-Remaining'].to_i).to eq(limit-2)
   end
 end
